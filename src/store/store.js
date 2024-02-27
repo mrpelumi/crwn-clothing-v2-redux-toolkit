af975c90d585621d67f8315a2f66ba5fe328b1c5
mrpelumi <oguntolapelumi6@gmail.com>
2024-02-27 15:17:59 +0000

    Added stripe payment

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,4 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -9,26 +7,8 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleWares)
+});
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
-
-export const persistor = persistStore(store);
